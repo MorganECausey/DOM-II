318a79a6e807ec9a7088af83a37483db9bcb0b1f
Morgan Causey <morganelizabethcausey@gmail.com>
2023-07-28 16:18:13 -0400

    work in progress, adding first event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './less/index.less'
 
 // Your code goes here!
+//LISTENERS
+//load
+window.onload = function (event) {
+    const heading = document.querySelector('h1')
+    console.log(heading)
+    heading.textContent = 'READY TO GO!!'
+    window.addEventListener('copy', () => {
+        navigator.clipboard.readText()
+            .then(text => {
+                console.log(text)
+                heading.textContent =+ text
+            })
+    })
+}
+//copy event
